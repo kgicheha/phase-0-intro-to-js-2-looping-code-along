8e13d63ac9a93fc4c5e91bfa2432a678d5349976
Kevin Gicheha <gichehakevin@gmail.com>
2022-06-07 19:56:30 -0700

    commit tree 08af0c74b9cd416ca5b2c2871398485c4238f9aa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    let messageArr = []
+    for(let i = 0; i < arr.length; i++ ) {
+        messageArr.push( `Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return messageArr;
+}
+
+function countDown () {
+    for (let i = 0; i <11; i++) {
+        console.log(i);
+    }
+}
